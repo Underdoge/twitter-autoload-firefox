2677135eaa2f67dddaeeb0c908fb9f9b960a713a
Underdoge <echapa@live.com.mx>
2017-08-17 18:33:51 -0500

    Add files via upload

---
+++ twitter-autoload.js
@@ -0,0 +1,25 @@
+var target = document.getElementsByClassName('stream-item js-new-items-bar-container');
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+ mutations.forEach(function(mutation) {
+   if(window.scrollY <= 10){
+    mutation.target.getElementsByClassName('new-tweets-bar js-new-tweets-bar')[0].click();
+   }
+   else
+    console.log('scroll at: '+window.scrollY);
+ });    
+});
+
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true };
+
+// pass in the target node, as well as the observer options
+observer.observe(target[0], config);
+
+// later, you can stop observing
+//observer.disconnect();
+
+//new-tweets-bar js-new-tweets-bar
+
+//https://developer.mozilla.org/en/docs/Web/API/MutationObserver

e0641ef576c4a3ff27385c9f2b0d16ef1930e1db
Underdoge <echapa@live.com.mx>
2017-08-18 00:52:30 -0500

    fixed bug

--- twitter-autoload.js
+++ twitter-autoload.js
@@ -1,19 +1,17 @@
-var target = document.getElementsByClassName('stream-item js-new-items-bar-container');
+var target = document.getElementsByTagName('body');
 
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        if(window.scrollY <= 10 && mutation.target.getElementsByClassName('new-tweets-bar js-new-tweets-bar').length > 0){
+        if(window.scrollY <= 10 && mutation.target.getElementsByClassName('new-tweets-bar js-new-tweets-bar').length > 0)
             mutation.target.getElementsByClassName('new-tweets-bar js-new-tweets-bar')[0].click();
-        }
-    });    
+    });
 });
 
-var config = { attributes: true, childList: true, characterData: true };
+var config = { childList: true, subtree: true };
 
 observer.observe(target[0], config);
 
 document.addEventListener('scroll', function(e) {
-    if (window.scrollY === 0 && document.getElementsByClassName('new-tweets-bar js-new-tweets-bar').length > 0) {
+    if (window.scrollY === 0 && document.getElementsByClassName('new-tweets-bar js-new-tweets-bar').length > 0)
         document.getElementsByClassName('new-tweets-bar js-new-tweets-bar')[0].click();
-    }
 });

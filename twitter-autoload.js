d18abeff5b7be4888a8b198538161943eaf6949d
Underdoge <echapa@live.com.mx>
2017-08-17 22:02:37 -0500

    Update twitter-autoload.js

--- twitter-autoload.js
+++ twitter-autoload.js
@@ -11,3 +11,10 @@ var observer = new MutationObserver(function(mutations) {
 var config = { attributes: true, childList: true, characterData: true };
 
 observer.observe(target[0], config);
+
+document.addEventListener('scroll', function(e) {
+	console.log('scroll detected');
+    if (window.scrollY === 0 && document.getElementsByClassName('new-tweets-bar js-new-tweets-bar').length > 0) {
+		  document.getElementsByClassName('new-tweets-bar js-new-tweets-bar')[0].click();
+		}
+});

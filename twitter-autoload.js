b31b291cb990475d7fec9f017c23c50032781d10
Underdoge <echapa@live.com.mx>
2022-03-02 15:10:22 -0600

    Updated for new Twitter design

--- twitter-autoload.js
+++ twitter-autoload.js
@@ -3,17 +3,11 @@ var scrollPos = window.scrollY;
 
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        if (mutation.target.className === "css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-1sp51qo r-o7ynqc r-6416eg"){ //windows
-            if (scrollPos === 0 && scrollPos < window.scrollY && window.location.href.includes("twitter.com/home"))
+        if (mutation.target.className === "css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"){ //windows
+            if (scrollPos === 0)
                 mutation.target.click();
             
-        } else
-            if (mutation.target.className === "css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-1bq9uc5 r-o7ynqc r-6416eg") //mac
-                if (scrollPos === 0 && scrollPos < window.scrollY && window.location.href.includes("twitter.com/home"))
-                    mutation.target.click();
-                
-        if (window.scrollY <= 10 && mutation.target.className === 'new-tweets-bar js-new-tweets-bar') //old design
-            mutation.target.click();
+        }
     });
 });
 
@@ -23,6 +17,6 @@ observer.observe(target, config);
 
 document.addEventListener('scroll', function(e) {
     scrollPos = window.scrollY;
-    if (window.scrollY === 0 && document.getElementsByClassName('new-tweets-bar js-new-tweets-bar').length > 0) // old design
-        document.getElementsByClassName('new-tweets-bar js-new-tweets-bar')[0].click();
+    if (window.scrollY === 0) // old design
+        document.getElementsByClassName('css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0')[0].click();
 });

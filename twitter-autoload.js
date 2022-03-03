c172fc119d78d09dbdcfb24a6ead04dc75d1f053
Underdoge <echapa@live.com.mx>
2022-03-02 19:12:43 -0600

    Fixed bug

--- twitter-autoload.js
+++ twitter-autoload.js
@@ -17,6 +17,6 @@ observer.observe(target, config);
 
 document.addEventListener('scroll', function(e) {
     scrollPos = window.scrollY;
-    if (window.scrollY === 0) // old design
+    if (window.scrollY === 0 && document.getElementsByClassName('css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0').length > 0) // old design
         document.getElementsByClassName('css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0')[0].click();
 });
